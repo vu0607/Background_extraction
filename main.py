11e6b2066ec6c0224f19bda5e24985f8cebc21c2
vu0607 <vunguyendang12a1.vhien@gmail.com>
2022-07-11 17:09:24 +0700

    feat: Correct all def format and saving result image path

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import os
 import argparse
 
 
-class Extraction:
-    def __init__(self):
-        pass
+class Extractor:
+    def __init__(self, kernel_size=(5, 5)):
+        self.kernel_size = kernel_size
 
+    @staticmethod
     def cvt_bgr_to_gray_img(img: np.ndarray):
         """
         Converting BGR image to Gray image
@@ -17,26 +18,30 @@ class Extraction:
         gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         return gray_img
 
+    @staticmethod
     def cvt_gray_to_threshold_img(gray_img: np.ndarray):
         """
         Converting Gray image to threshold image
         :param gray_img : np.ndarray
         :return: thresh_img
         """
-        thresh_img = cv.threshold(gray_img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+
+        # w, h, channel = image.shape
+
+        _, thresh_img = cv.threshold(gray_img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
         return thresh_img
 
-    def dilating_threshold(thresh_img: np.ndarray):
+    def dilating_threshold(self, thresh_img: np.ndarray):
         """
         Dilating threshold image
         :param thresh_img : np.ndarray
         :return: mask
         """
-        kernel_size = (5, 5)
-        kernel = np.ones(kernel_size, "uint8")
+        kernel = np.ones(self.kernel_size, "uint8")
         mask = cv.dilate(thresh_img, kernel)
         return mask
 
+    @staticmethod
     def painting_image_with_mask(image: np.ndarray, mask: np.ndarray):
         """
         Painting image with mask
@@ -47,15 +52,15 @@ class Extraction:
         result_image = cv.inpaint(image, mask, 7, cv.INPAINT_NS)
         return result_image
 
-    def extraction(image: np.ndarray):
+    def extract(self, image: np.ndarray):
         """
         Extract background from image
         :param image: np.ndarray
         """
-        return Extraction.painting_image_with_mask(image,
-                                                   Extraction.dilating_threshold(
-                                                       Extraction.cvt_gray_to_threshold_img(
-                                                           Extraction.cvt_bgr_to_gray_img(image))))
+        return self.painting_image_with_mask(image,
+                                             self.dilating_threshold(
+                                                 self.cvt_gray_to_threshold_img(
+                                                     self.cvt_bgr_to_gray_img(image))))
 
 
 def parser_args():
@@ -77,11 +82,12 @@ def main(args):
     """
     input_folder = args.input_folder
     result_folder = args.output_folder
+    extraction = Extractor()
     for img in os.listdir(input_folder):
         img_path = os.path.join(input_folder, img)
         image = cv.imread(img_path)
-        result_image = Extraction.extraction(image)
-        cv.imwrite(result_folder + '/' + str(img), result_image)
+        result_image = extraction.extract(image)
+        cv.imwrite(result_folder + os.sep + img, result_image)
 
 
 if __name__ == '__main__':
